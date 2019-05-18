5da0914c14c3841243a1673f5a3f8a037b30babd
Cristian Espinoza <ceegarner@gmail.com>
2019-05-18 00:54:58 -0400

    updated frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import Web3 from "web3";
 import Fortmatic from "fortmatic";
 import "./App.css";
+import Header from "./components/Header";
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -32,6 +33,7 @@ class App extends Component {
       }
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
@@ -56,7 +58,6 @@ class App extends Component {
 
   runExample = async () => {
     const { accounts, contract } = this.state;
-    console.log(this.state);
 
     // Stores a given value, 5 by default.
     await contract.methods.set(5).send({ from: accounts[0] });
@@ -69,23 +70,12 @@ class App extends Component {
   };
 
   render() {
+    const { accounts, contract } = this.state;
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+      <Header account={accounts[0]} web3={window.web3} contracts={contract} />
     );
   }
 }
