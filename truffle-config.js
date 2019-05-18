12c41461216c58c7ec96c1b2514d2c005664606a
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2019-05-18 10:21:58 -0400

    Adds Contract integration for trading tokens

--- truffle-config.js
+++ truffle-config.js
@@ -6,15 +6,19 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!compilers: {
 
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-   
-    development: {
+contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  works: {
+    
+      evelopment: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // match any network
       websockets: true
     },
-   
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.18"
+    }
+ }
 };
