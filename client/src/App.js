08fe31b6ba79aa14b6494457100602f0c2fcc7d4
Cristian Espinoza <ceegarner@gmail.com>
2019-05-18 00:00:44 -0400

    added fortmatic

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
-
+import Web3 from "web3";
+import Fortmatic from "fortmatic";
 import "./App.css";
 
 class App extends Component {
@@ -10,8 +10,26 @@ class App extends Component {
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      // const web3 = await getWeb3();
+      let fm = new Fortmatic("pk_test_C6808B2B488687F6");
+      let web3;
+      // Post EIP-1102 update which MetaMask no longer injects web3
+      if (window.ethereum) {
+        // Use MetaMask provider
+        web3 = new Web3(window.ethereum);
+      } else {
+        // Use Fortmatic provider
+        web3 = new Web3(fm.getProvider());
+      }
 
+      // Legacy dApp browsers which web3 is still being injected
+      if (typeof web3 !== "undefined") {
+        // Use injected provider
+        window.web3 = new Web3(web3.currentProvider);
+      } else {
+        // Use Fortmatic provider
+        window.web3 = new Web3(fm.getProvider());
+      }
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
@@ -20,16 +38,17 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      console.log(web3);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -37,6 +56,7 @@ class App extends Component {
 
   runExample = async () => {
     const { accounts, contract } = this.state;
+    console.log(this.state);
 
     // Stores a given value, 5 by default.
     await contract.methods.set(5).send({ from: accounts[0] });
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
-
+require("dotenv").config(); // Store environment-specific variable from '.env' to process.env
 const path = require("path");
-
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNENOMIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,13 +9,18 @@ module.exports = {
 
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-   
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // match any network
       websockets: true
     },
-   
+    rinkeby: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider(mnemonic, process.env.RINKEBY_API_URL),
+      network_id: "4",
+      skipDryRun: true
+    }
   }
 };
